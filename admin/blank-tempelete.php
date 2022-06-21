00bd3e570c4c348088dba490b1904b6a7354c6db
Shahidul Islam Faisal <sifaisalcox@gmail.com>
2022-06-21 14:53:21 +0600

    my admin

---
+++ admin/blank-tempelete.php
@@ -0,0 +1,37 @@
+
+ <?php include "include/header.php";?>
+
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Dashboard</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+            <li class="breadcrumb-item active"> Home</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+      <!-- Main content -->
+      <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+          <div class="col-lg-12 col-12">
+              <h1>Your content goes here</h1>
+          </div>
+        </div>
+      </div>
+      </section>
+  </div>
+  <!-- /.content-wrapper -->
+  <?php include "include/footer.php";?>
